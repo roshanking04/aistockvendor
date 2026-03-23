e4e98dbafab251dea8f88114add405c0416377f8
roshanking04 <107301394+roshanking04@users.noreply.github.com>
2026-03-23 09:26:59 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,34 +1,18 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
-import Footer from "./Footer";
-import WhatsAppButton from "./WhatsAppButton";
 import "./Layout.css";
 
-function Layout({ children }) {
+export default function Layout({ children }) {
   return (
     <div className="layout-root">
-      {/* Fixed Sidebar */}
       <Sidebar />
-
-      {/* Main content area */}
       <div className="layout-body">
-        {/* Sticky Topbar */}
         <Topbar />
-
-        {/* Page Content */}
         <main className="layout-main">
           {children}
         </main>
-
-        {/* Footer */}
-        <Footer />
       </div>
-
-      {/* Floating WhatsApp */}
-      <WhatsAppButton />
     </div>
   );
 }
-
-export default Layout;
